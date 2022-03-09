bb174017a4729ab9d0752402ed9081228c58e654
Guilherme <guih_so@hotmail.com>
2022-03-08 23:06:18 -0300

    adicionando o valor em bitcoins

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ function Converter(){
 
     let valorEuro = document.querySelector('#conversor');
     let valor = valorEuro.value;
-    let euroHoje = 5.52
+    let euroHoje = 5.52;
+    let bitcoinHoje = 35948.94;
     let valorEmReais = valor * euroHoje;
+    let valorEmBitcoin = valor / bitcoinHoje;
     
     document.querySelector('#resultado').innerHTML = `$ ${valorEmReais.toFixed(2)}`;
+    document.querySelector('#resultadoBitcoin').innerHTML = `&#8383; ${valorEmBitcoin.toFixed(5)}`;
 
     console.log(valorEmReais);
 }
